f9b721ed9b91c3ca71fcefaa5ee1f78a5bfc3d2a
baton96 <podolce@op.pl>
2022-06-29 10:23:42 +0200

    MvcNews

---
+++ 8 MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcNews.Models {
+    public class NewsItem {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+
+        [StringLength(140, MinimumLength=5)]
+        [Required]
+        public string Text { get; set; } = string.Empty;
+        
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+    }
+}
